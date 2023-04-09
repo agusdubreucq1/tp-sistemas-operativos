5327a6d2a7b03e947281f4f1d27e968284474e72
GuidoMartinez1 <guimartinez@frba.utn.edu.ar>
2023-04-09 20:41:14 +0000

    Agrego carpeta logs para almacenar los mismos (se tiene que pasar como parametro)

--- Consola/src/consola.c
+++ Consola/src/consola.c
@@ -6,7 +6,7 @@
 
 int main(int argc, char** argv) {
 
-    if (argc != 2) {
+    if (argc != 3) {
     	puts("\n No se introdujo la cantidad de argumentos necesarios"
     	        "\n   * Primer  argumento: <Archivo de configuracion>"
                 "\n   * Segundo argumento: <Archivo de log>");
